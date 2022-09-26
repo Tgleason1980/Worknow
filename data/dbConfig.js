f7e0e604c2712dc2758bd0d2f56b1d6d5f375102
Tgleason1980 <terryscoding@gmail.com>
2022-09-26 04:22:25 -0500

    modified:   data/dbConfig.js 	modified:   index.js

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const config = require('../knexfile')
 require('dotenv').config()
 require('pg')
-
+const db = knex(config.development);
 
 const dbEnv = process.env.DB_ENV || 'development'
 
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./api/server')
-
+require("dotenv").config(); // load .env variables
 const port = process.env.PORT || 9000
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
